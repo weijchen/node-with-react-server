232df07052e1304a277b0ea8e2ea54487a3466af
jimmyweicc <weijchen0919@gmail.com>
2020-09-19 17:54:58 +0800

    add google oauth code

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const express = require("express"),
-      app     = express(),
-      PORT    = process.env.PORT || 3000
+const express         = require("express"),
+      passport        = require("passport"),
+      GoogleStrategy  = require("passport-google-oauth20").Strategy,
+      keys            = require("./config/keys"),
+      app             = express(),
+      PORT            = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send({ hi: 'there' });
-});
+// console.developers.google.com
+passport.use(
+  new GoogleStrategy( // by default, setted as 'google' identifier
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] // asking google to give profile and email
+  })
+);
 
 app.listen(PORT, (req, res) => {
   console.log(`Server start at http://localhost:${PORT}`);
